a0970f90cb5238ea4141a1aad65b6aa5e4a9a5fb
Ardi Mirzaei <ardimir@gmail.com>
2022-04-12 16:21:48 +1000

    Add code for Rankings of 6666 and 6677

--- Analysis_Main.R
+++ Analysis_Main.R
@@ -13,8 +13,13 @@ source('Scripts/Phos_Load_and_Preprocess.R')
 
 source('Scripts/Phos_Alpha_vs_RMSD.R')
 
-
+# Top and bottom 5
 
 rmarkdown::render(input = 'Scripts/Phos_Top5Bottom5_Tables.rmd',
                   output_file = paste0('TopAndBottom_Tables_',format(Sys.time(), '%Y%m%d'),'.docx'),
+                  output_dir = 'Analysis/')
+
+# Ranking of nl = 6666 and 6677
+rmarkdown::render(input = 'Scripts/Phos_Rankings_6666_6677.rmd',
+                  output_file = paste0('Rankings_6666_6677_',format(Sys.time(), '%Y%m%d'),'.docx'),
                   output_dir = 'Analysis/')
