d709746c0d29f8def0d8a8d84624deb1fe90456b
ardimirzaei <45409241+ardimirzaei@users.noreply.github.com>
2022-10-21 09:36:57 +1100

    Update RunExport.bat

--- RunExport.bat
+++ RunExport.bat
@@ -1,4 +1,2 @@
-rem python3 File_Processing_For_Analysis.py
-echo blank > .zipit
-rem echo del > .zipit
+python3 File_Processing_For_Analysis.py
 C:\"Program Files"\R\R-4.1.3\bin\Rscript Analysis_Main.R
